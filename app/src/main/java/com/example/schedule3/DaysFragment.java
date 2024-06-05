40c8a253f0557deeb5b6abe23c0a3b0be46d03f2
ghostwalker18 <76010772+ghostwalker18@users.noreply.github.com>
2024-06-06 00:52:36 +0300

    transfered main screen

--- app/src/main/java/com/example/schedule3/DaysFragment.java
+++ app/src/main/java/com/example/schedule3/DaysFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Date;
+import java.util.Observer;
+import java.util.Vector;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
@@ -14,20 +18,58 @@ public class DaysFragment extends Fragment implements SharedPreferences.OnShared
    private SharedPreferences prefs;
    private View view;
    private ScheduleState state;
+   private Vector<ScheduleItemFragment> days = new Vector<>();
 
    @Override
    public void onCreate(@Nullable Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
       prefs.registerOnSharedPreferenceChangeListener(this);
+      state = new ScheduleState(new Date());
+      days.add(ScheduleItemFragment.newInstance(
+              state.getYear(),
+              state.getWeek(),
+              R.string.monday));
+      days.add(ScheduleItemFragment.newInstance(
+              state.getYear(),
+              state.getWeek(),
+              R.string.tuesday));
+      days.add(ScheduleItemFragment.newInstance(
+              state.getYear(),
+              state.getWeek(),
+              R.string.wednesday));
+      days.add(ScheduleItemFragment.newInstance(
+              state.getYear(),
+              state.getWeek(),
+              R.string.thursday));
+      days.add(ScheduleItemFragment.newInstance(
+              state.getYear(),
+              state.getWeek(),
+              R.string.friday));
+      for (ScheduleItemFragment day:days) {
+         state.addObserver((Observer)day);
+      }
+   }
+
+   @Nullable
+   @Override
+   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+      return inflater.inflate(R.layout.fragment_days, container, false);
    }
 
    @Override
-   public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                            Bundle savedInstanceState) {
-      view = inflater.inflate(R.layout.fragment_days, container, false);
-      setUpTeacherSearch(view, prefs);
-      return view;
+   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+      super.onViewCreated(view, savedInstanceState);
+      this.view = view;
+      setUpTeacherSearch(this.view, prefs);
+      view.findViewById(R.id.forward_button).setOnClickListener(v -> {state.goNextWeek();});
+      view.findViewById(R.id.back_button).setOnClickListener(v -> {state.goPreviousWeek();});
+      for(ScheduleItemFragment day: days){
+         getParentFragmentManager()
+                 .beginTransaction()
+                 .add(R.id.days_container, day)
+                 .commit();
+      }
    }
 
    @Override
--- app/src/main/java/com/example/schedule3/ScheduleItemFragment.java
+++ app/src/main/java/com/example/schedule3/ScheduleItemFragment.java
@@ -1,22 +1,43 @@
 package com.example.schedule3;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 import org.json.JSONObject;
 
-public class ScheduleItemFragment extends Fragment {
+public class ScheduleItemFragment extends Fragment implements Observer {
+   private static HashMap<Integer, Integer> weekdaysNumbers = new HashMap<>();
+   static {
+      weekdaysNumbers.put(R.string.monday, Calendar.MONDAY);
+      weekdaysNumbers.put(R.string.tuesday, Calendar.TUESDAY);
+      weekdaysNumbers.put(R.string.wednesday, Calendar.WEDNESDAY);
+      weekdaysNumbers.put(R.string.thursday, Calendar.THURSDAY);
+      weekdaysNumbers.put(R.string.friday, Calendar.FRIDAY);
+   }
+   private View view;
+   private TableLayout table;
+   private Calendar date;
+   private int year;
+   private int week;
+   private int dayOfWeekID;
+   private boolean isOpened = false;
 
-   public static ScheduleItemFragment newInstance(int year, int week, String dayOfWeek) {
+   public static ScheduleItemFragment newInstance(int year, int week, int dayOfWeekId) {
       Bundle args = new Bundle();
       args.putInt("year", year);
       args.putInt("week", week);
-      args.putString("dayOfWeek", dayOfWeek);
+      args.putInt("dayOfWeek", dayOfWeekId);
       ScheduleItemFragment fragment = new ScheduleItemFragment();
       fragment.setArguments(args);
       return fragment;
@@ -24,6 +45,13 @@ public class ScheduleItemFragment extends Fragment {
    @Override
    public void onCreate(@Nullable Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+      dayOfWeekID = getArguments().getInt("dayOfWeek");
+      week = getArguments().getInt("week");
+      year = getArguments().getInt("year");
+      date = new Calendar.Builder().setWeekDate(year, week, weekdaysNumbers.get(dayOfWeekID)).build();
+      if(isDateToday(date)){
+         isOpened = true;
+      };
    }
 
    @Override
@@ -32,7 +60,56 @@ public class ScheduleItemFragment extends Fragment {
       return inflater.inflate(R.layout.fragment_schedule_item, container, false);
    }
 
-   public void setSchedule(JSONObject schedule){
+   @Override
+   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+      super.onViewCreated(view, savedInstanceState);
+      this.view = view;
+      Button button = view.findViewById(R.id.button);
+      button.setText(generateTitle(date, dayOfWeekID));
+      button.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+      table = view.findViewById(R.id.schedule);
+      if(isOpened){
+         table.setVisibility(View.VISIBLE);
+      }
+      else{
+         table.setVisibility(View.GONE);
+      }
+   }
+
+   private String generateTitle(Calendar date,  int dayOfWeekId){
+      String dayOfWeek = getResources().getString(dayOfWeekId);
+      //Month is a number in 0 - 11
+      int month = date.get(Calendar.MONTH) + 1;
+      //Formatting month number with leading zero
+      String monthString = String.valueOf(month);
+      if(month < 10){
+         monthString = "0" + monthString;
+      }
+      int day = date.get(Calendar.DAY_OF_MONTH);
+      String dayString = String.valueOf(day);
+      //Formatting day number with leading zero
+      if(day < 10){
+         dayString = "0" + dayString;
+      }
+      String label = dayOfWeek + " (" + dayString  + "/" + monthString + ")";
+      if(isDateToday(date)){
+         label += " - Сегодня";
+      }
+      return label;
+   }
+
+   private boolean isDateToday(Calendar date){
+      Calendar rightNow = Calendar.getInstance();
+      if(rightNow.get(Calendar.YEAR) == date.get(Calendar.YEAR)
+              && rightNow.get(Calendar.MONTH) == date.get(Calendar.MONTH)
+              && rightNow.get(Calendar.DAY_OF_MONTH) == date.get(Calendar.DAY_OF_MONTH)){
+         return true;
+      }
+      return false;
+   }
+
+   @Override
+   public void update(Observable observable, Object o) {
 
    }
 }
