95355c8cff7b93cc64cb5e26ed8adbf179203b85
ghostwalker18 <76010772+ghostwalker18@users.noreply.github.com>
2025-01-22 17:02:35 +0300

    added import/export db activity with resources

---
+++ app/src/main/java/com/ghostwalker18/schedule/views/ImportActivity.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ghostwalker18.schedule.views;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import com.ghostwalker18.schedule.R;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Этот класс используется для отображенияя экрана импорта и экспорта БД приложения.
+ *
+ * @author Ипатов Никита
+ * @since 4.1
+ */
+public class ImportActivity
+        extends AppCompatActivity {
+   private Spinner operationTypeSpinner;
+   private Spinner dataTypesSpinner;
+   private Spinner importModeSpinner;
+   private Button doOperationButton;
+   @Override
+   protected void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_import);
+      doOperationButton = findViewById(R.id.do_operation);
+      doOperationButton.setOnClickListener(v -> exportDB());
+      operationTypeSpinner = findViewById(R.id.operation_type);
+      dataTypesSpinner = findViewById(R.id.data_types);
+      importModeSpinner = findViewById(R.id.import_policy_type);
+      operationTypeSpinner.setOnItemClickListener((adapterView, view, i, l) -> {
+         String operation = operationTypeSpinner.getSelectedItem().toString();
+         if(operation.equals("import")){
+            findViewById(R.id.import_options_panel).setVisibility(View.VISIBLE);
+            doOperationButton.setText(R.string.import_data);
+            doOperationButton.setOnClickListener(v -> importDB());
+         } else {
+            findViewById(R.id.import_options_panel).setVisibility(View.GONE);
+            doOperationButton.setText(R.string.export_data);
+            doOperationButton.setOnClickListener(v -> exportDB());
+         }
+      });
+   }
+
+   /**
+    * Этот метод используется для экспорта БД приложения.
+    */
+   private void exportDB(){
+      String dataType = dataTypesSpinner.getSelectedItem().toString();
+   }
+
+   /**
+    * Этот метод используется для импорта БД приложения.
+    */
+   private void importDB(){
+      String importPolicy = importModeSpinner.getSelectedItem().toString();
+      String dataType = dataTypesSpinner.getSelectedItem().toString();
+   }
+}
--- app/src/main/java/com/ghostwalker18/schedule/views/SettingsActivity.java
+++ app/src/main/java/com/ghostwalker18/schedule/views/SettingsActivity.java
@@ -16,6 +16,7 @@ package com.ghostwalker18.schedule.views;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -64,11 +65,11 @@ public class SettingsActivity
      */
     private boolean sendEmailToDeveloper(View view){
         try{
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
             intent.putExtra(Intent.EXTRA_EMAIL, new String[]{ScheduleApp.DEVELOPER_EMAIL});
-            intent.putExtra(Intent.EXTRA_SUBJECT, "Расписание ПАСТ (мобильное)");
-            startActivity(intent);
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+            startActivity(Intent.createChooser(intent, getString(R.string.connect_to_developer)));
         } catch (ActivityNotFoundException e){
             Toast toast = new Toast(this);
             toast.setText(R.string.no_email_client_found);
